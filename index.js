0c38a3e782b9963eb83c179bcdc63b7f82c742e4
Alejandro Sánchez Pineda <alsapi2004@gmail.com>
2020-02-14 14:05:04 +0100

    set torrent result to db

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * Starter point of the application
  */
 const schedule = require('./src/schedule');
+const search = require('./src/search');
 const debug = require('debug')('torrent-auto-downloader');
 
 // arguments passed
@@ -14,6 +15,7 @@ async function start() {
   try {
     await schedule.update();
     const availableEpisodes = await schedule.getAvailableEpisodes();
+    await search.searchEpisodes(availableEpisodes);
 
   } catch (err) {
     debug(err)
--- src/search/index.js
+++ src/search/index.js
@@ -3,6 +3,7 @@
  */
 const fs = require('fs-extra');
 const debug = require('debug')('searchTorrent');
+const DB = require('../database');
 
 /**
  * Configure torrent search
@@ -12,39 +13,50 @@ const torrentSearch = require('torrent-search-api');
 torrentSearch.enablePublicProviders();
 
 /**
- * Searches torrents with given params and chooses the biggest found
+ * Searches torrent with given params and chooses the biggest found
  * @todo Torrent selection rules
- * @param {String} show name
- * @param {String} season
- * @param {String} episode
+ * @param {Object} episode
+ * @returns episode with search result
  */
-async function searchTorrent(show, season, episode) {
-  const limit = 20;
+async function _searchEpisode(episode) {
+  const {showTitle, season: s, episode: e} = episode;
+  const episodeId = `${showTitle} season ${s} episode ${e}`;
 
-  debug(`Searching ${show}...`);
-  // const torrents = await torrentSearch.search(show, 'All', limit);
+
+  debug(`Searching ${episodeId}...`);
+  // const torrents = await torrentSearch.search(showTitle, 'All', limit);
   const torrents = JSON.parse(fs.readFileSync('./src/search/torrents.json'));
-
-  const showRegex = new RegExp(`.*(s0?${season}e0?${episode}).*`, 'gi');
-
+  const showRegex = new RegExp(`.*(s0?${s}e0?${e}).*`, 'gi');
   const filteredTorrents = torrents.filter(torrent => showRegex.test(torrent.title));
 
+
+
+  debug(`${filteredTorrents.length} torrents found for ${episodeId}...`);
+
   if(filteredTorrents.length) {
-    debug(`${filteredTorrents.length} torrents found. Choosing highest.`);
+    const selectedTorrent = _getHighestSize(filteredTorrents);
 
-    // @todo database module
-    const showPath = `./data/selectedShows/${show}.json`;
-    const showData = JSON.parse(await fs.readFile(showPath));
-
-    showData[season][episode].torrents = filteredTorrents.sort(
-      (a,b) => parseSize(a.size) - parseSize(b.size)
-    );
-
-    await fs.writeJson(showPath, showData, {spaces: 2});
+    DB.get('episodes').find({
+      showTitle, season: s, episode: e
+    }).set('torrent', selectedTorrent).write();
   }
 }
 
-function parseSize(size) {
+/**
+ * Returns torrent with highest size
+ * @param {Array} torrents
+ */
+function _getHighestSize(torrents) {
+  return torrents.reduce((prev, act) =>
+    _parseSize(prev) > _parseSize(act) ? prev : act
+  );
+}
+
+/**
+ * @param {String} size 1.4 Gib | 333 MB | ....
+ * @return number of MB
+ */
+function _parseSize(size) {
   const isGbRegEx = /Gi?B/;
   const isMbRegEx = /Mi?B/;
 
@@ -55,4 +67,20 @@ function parseSize(size) {
   } else return size;
 }
 
-searchTorrent('Doctor Who', '12', '06');
+
+/**
+ *
+  * Searches torrents of all given episodes
+  * @param {Array} episodes to search
+  * @returns search results
+  */
+async function searchEpisodes(episodes) {
+  debug('Searching torrentless episodes');
+  episodes = episodes.filter(episode => !episode.torrent);
+
+  await Promise.all(episodes.map(_searchEpisode));
+}
+
+module.exports = {
+  searchEpisodes
+}
